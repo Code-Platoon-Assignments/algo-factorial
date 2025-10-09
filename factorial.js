3ee07c8f7621541296e3e43aa9f7fea59ff1da15
Samuel Bordenkecher <stb4380@gmail.com>
2025-10-08 20:35:55 -0400

    Completed Factorial

--- factorial.js
+++ factorial.js
@@ -1,5 +1,31 @@
-function factorial(num) {
-  return 0;
-}
 
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  }
+  else if (num === 1) {
+    return 1;
+  }
+  else if (num > 1) {
+     total = 1;
+     let setNum = num
+     for (let i = 0; i < setNum; i++) {
+      
+      total *= num;
+      //console.log('total ' + total)
+      num = (num - 1);
+      //console.log('num ' + num)
+    }
+    return total
+  }
+}
 module.exports = factorial;
+
+
+/** NOTES
+ * Edge Cases = 0, negatives
+ * turn num into str
+ * split string apart
+ * multiply strings together
+ * repreat until num is single
+ */
