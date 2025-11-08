6f28e13b942181e404320e790b6e1ec62c39c9c8
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-08 12:03:24 -0700

    Factorial v1

--- factorial.js
+++ factorial.js
@@ -1,5 +1,12 @@
 function factorial(num) {
-  return 0;
+    let fact = 1;
+    for (let f = 1; f <= num; f++) {    // for loop; f is an iterator; continues as long as f <= num; f++ counts for each iteration
+        fact *= f;
+    }
+    return fact; 
 }
 
+// console.log(factorial(10));  // for testing
+
 module.exports = factorial;
+
