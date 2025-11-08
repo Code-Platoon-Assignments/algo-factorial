aab6d1bd5130774080b963890dc62d34a8338dd8
weaponx69 <perry.brian@gmail.com>
2025-11-08 14:05:12 -0500

    Algo to do factorials

--- factorial.js
+++ factorial.js
@@ -1,5 +1,16 @@
-function factorial(num) {
-  return 0;
+function factorial(num) 
+{
+  let result = 1;
+
+  // start with the number and multiply down to 1
+  for(let i=num; i>1; i--)
+  {
+    // num = 4(4), 3(12), 2(24), 1(24)
+    // 4, 12, 24, 24
+    result *= i;
+  }
+  
+  return result;
 }
 
 module.exports = factorial;
