6f333dcfa9ab1a5fbea46607145fb8d1fddd8ec6
VaNiesha Honani <vhonani77@gmail.com>
2025-11-08 10:30:21 -0800

    factorial assignment

--- factorial.js
+++ factorial.js
@@ -1,5 +1,23 @@
 function factorial(num) {
-  return 0;
+    if(num < 0){
+        return "This function only accepts non-negative numbers."
+    }else if(num===0 || num === 1){
+            return 1;
+    }else{
+        let factorial = 1;
+        for(let i = 2; i <= num; i++){
+            factorial *=i
+        }
+        return factorial
+    }
 }
 
-module.exports = factorial;
+function factorial_recursive(num){
+    if(num=== 0 || num === 1){
+        return 1
+    }
+    
+    return num * factorial_recursive(num-1);
+}
+
+module.exports = {factorial, factorial_recursive};
