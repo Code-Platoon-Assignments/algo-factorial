ee9eece5ec813657928c6554d27fbac61923a99b
VaNiesha Honani <vhonani77@gmail.com>
2025-11-08 17:36:22 -0800

    moved files out of repo and into other folder

--- lazy_numeralSpec.js
+++
@@ -1,6 +0,0 @@
-const { toRomanLazy, toRoman } = require("./lazy_numeral.js");
-
-console.log(toRomanLazy(6));
-console.log(toRomanLazy(10));
-console.log(toRomanLazy(11));
-console.log(toRomanLazy(20));
