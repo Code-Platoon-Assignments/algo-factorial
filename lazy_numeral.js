8f37b0870fc7c838d6ea681481554563ebfe8672
VaNiesha Honani <vhonani77@gmail.com>
2025-11-08 17:22:19 -0800

    romanNumeral solution

--- lazy_numeral.js
+++ lazy_numeral.js
@@ -1,4 +1,4 @@
-function toRomanLazy(num){
+function toRomanLazy(num){ //6
     
     const romanNumeralToArabic = {
         I: 1,
@@ -11,23 +11,44 @@ function toRomanLazy(num){
     }
     
     const romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V','I'];
-    const romanNumArray = []
+    const romanNumArray = [] //stores the converted files
     
-    for(let letter of romanNumeralPriorityOrder){
-        const value = romanNumeralToArabic[letter]
+    for(let letter of romanNumeralPriorityOrder){  //iterate through each letter
         
-        const romanModArabic = num % value 
-        console.log(`This is ${romanModArabic} `)
-        const count_floor = Math.floor(num / value)
-        console.log(`This is the floor ${count_floor}`)
+        const value = romanNumeralToArabic[letter] // get key - letter 'M' to start
+        
+        const romanModArabic = num % value // 6%1000 = remainder 6
+        const count_floor = Math.floor(num / value) 6/1000 = 0 
         if(count_floor > 0){
-            romanNumArray.push(letter.repeat(count_floor)) //get first prefix then iterate through based on count_floor 
-        }
-        
-        num = romanModArabic; //after first pass - the num will be updated with first modulo value so 
+            romanNumArray.push(letter.repeat(count_floor)) // first iteration will be 0 so M is not pushed
+        } 
+        //will continue on 
+        num = romanModArabic; // first iteration = 6  
         if(num == 0) break;
     }
     return romanNumArray.join('');
 }
 module.exports = {toRomanLazy}
-    
+
+
+//walk-thru of 6
+//Iteration 1
+/* num = 6
+value = M
+romanModArabic = 6%1000 -> 6
+count_floor = Math.floor(6/1000) -> 0
+if counter floor > 0 -> false
+num = romanModArabic -> 6
+
+//Fast forward to iteration 6 - containing 'V'
+num = 6
+value = V (5)
+romanModArabic = 6 % 5 -> remainder 1
+count_floor = 6/5 -> 1
+if count_floor > 0 -> true so enter loop 
+romanNumArray.push(letter(V).repeat(counter_floor)) -> V + count_floor) so repeat this 1 times
+
+num = romanModArabic -> 1
+
+go back to the top --> 1 
+*/ 
---
+++ psuedo_code_lazyRomanNums.md
@@ -0,0 +1,11 @@
+##psuedo code walk thru for toRomanLazy
+
+call function toRomanLazy(num) from lazy_numeralSpec.js
+pass in a number for example 7 
+
+
+## Solution in lazy_numeral.js
+
+//function
+function toRomanLazy(num) -> num = 6
+
