3718349bd18231cc7de410e819b62337459cb0b2
LVRG-creator <blivrago@gmail.com>
2026-02-04 12:58:57 -0500

    completed factorial challenge

--- factorial.js
+++ factorial.js
@@ -1,5 +1,18 @@
 function factorial(num) {
-  return 0;
+  //have to think about 0
+  if (num === 0) return 1; 
+
+  // then need a starting point, 1 just makes sense because 0 would be bad
+  let result = 1;
+
+  // then need a loop, start at 'num' and as long as i is a positive number, multiply it by i
+  ///then subtract 1 from i
+  for (let i = num; i > 1; i--) {
+    result = result * i;}
+
+    // give answer 
+  return result;
 }
+
 
 module.exports = factorial;
